1f055d56abf9050545a055dd81757566565160b5
Joshua Wales <joshua.wales@gmail.com>
2014-06-11 09:38:15 +1000

    Started adding rooms.

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,12 +1,19 @@
 var serverAddr = "10.1.1.4";
 var socket;
 var userName = prompt("Please enter a nickname", "Bogan");
+var activeRoom = "";
 
 connect();
 
 $('#inputText').bind('keypress', function(e) {
     if(e.keyCode==13){
 	sendMessage();
+    }
+});
+
+$('#inputText_room').bind('keypress', function(e) {
+    if(e.keyCode==13){
+	sendMessageRoom();
     }
 });
 
@@ -20,8 +27,10 @@ function connect(){
     });
 
     socket.on('message', function(data){
-	console.log("test");
-	$("#messageArea").val( data + "\n" + $("#messageArea").val());
+	if(data.roomName == "public")
+	    $("#messageArea").val( data.message + "\n" + $("#messageArea").val());
+	else
+	    $("#messageArea_room").val( data.message + "\n" + $("#messageArea").val());
     });
 
     socket.on('userList', function(data){
@@ -29,9 +38,29 @@ function connect(){
 	for(var i=0;i<data.clients.length;i++)
 	    $("#usersArea").val( $("#usersArea").val()+"\n"+data.clients[i] );
     });
+    
+    socket.on('userListRoom', function(data){
+	$("#usersArea_room").val("");
+	for(var i=0;i<data.clients.length;i++)
+	    $("#usersArea_room").val( $("#usersArea_room").val()+"\n"+data.clients[i] );
+    });
+
+    socket.on('activeRoom', function(data){
+	setRoomName(data);
+    });
 }
 
+function setRoomName(name){
+    activeRoom = name;
+    $("#roomName").text(name);
+}
 
 function sendMessage(){
-    socket.emit('sendMessage', $("#inputText").val());
+    var msg = $("#inputText").val();
+    socket.emit('sendMessage', { roomName: "public", message: msg });
+}
+
+function sendMessageRoom(){
+    var msg = $("#inputText_room").val();
+    socket.emit('sendMessage', { roomName: activeRoom, message: msg });
 }
--- server.js
+++ server.js
@@ -4,7 +4,13 @@ server = require('http').createServer(app),
 io = require('socket.io').listen(server),
 port = 8080;
 
+function Room(name){
+    this.name = name;
+    this.clients = new Object();
+}
+
 var clients = new Object();
+var rooms = new Object();
 
 server.listen(port);
 
@@ -18,30 +24,69 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
 
     socket.emit('connect');
-    socket.emit('userList', { clients: getClientList() });
+    socket.emit('userList', { clients: getClientList(clients) });
 
     socket.on('user', function (data) {
 	console.log(data+" logged in");
 	clients[socket.id] = data;
-	io.sockets.emit('userList', { clients: getClientList() });
+	io.sockets.emit('userList', { clients: getClientList(clients) });
     });
 
     socket.on('sendMessage', function (data) {
-	io.sockets.emit('message', clients[socket.id]+" : "+data);
+	var msg = clients[socket.id]+" : "+data.message;
+	io.sockets.emit('message', { roomName: data.roomName, message: msg } );
+    });
+
+    socket.on('createRoom', function (data) {
+	if( rooms[data] === undefined ){
+	    createRoom(data);
+	    rooms[data] = new Room(data);
+	    rooms[data].clients[socket.id] = clients[socket.id];
+	    io.sockets.emit('userListRoom', { roomname: data, clients: getClientList(rooms[key].clients) });
+	    socket.emit('activeRoom', data);
+	}
+    });
+
+    socket.on('selectRoom', function (data) {
+	if( rooms[data] !== undefined ){
+	    removeClient_Rooms(id);
+	    rooms[data].clients[socket.id] = clients[socket.id];
+	    socket.emit('activeRoom', data);
+	}
     });
 
     socket.on('disconnect', function(){
 	console.log(clients[socket.id]+" logged out");
-	delete clients[socket.id];
-	io.sockets.emit('userList', { clients: getClientList() });
+	removeClient(socket.id);
     });
 
 });
 
-function getClientList(io){
+function removeClient(id){
+    delete clients[id];
+    removeClient_Rooms(id);
+    io.sockets.emit('userList', { clients: getClientList(clients) });
+}
+
+function removeClient_Rooms(id){
+    Object.keys(rooms).forEach(function (key) { 
+	delete rooms[key].clients[id];
+	io.sockets.emit('userListRoom', { roomname: key, clients: getClientList(rooms[key].clients) });
+    })
+}
+
+function getClientList(list){
     var names = [];
-    Object.keys(clients).forEach(function (key) { 
-	names.push(clients[key]);
+    Object.keys(list).forEach(function (key) { 
+	names.push(list[key]);
+    })
+    return names
+}
+
+function getRoomList(){
+    var names = [];
+    Object.keys(rooms).forEach(function (key) { 
+	names.push(rooms[key].name);
     })
     return names
 }
